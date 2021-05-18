a26547286637a53710a71559eba10e06160b4673
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-05-18 23:43:33 +0530

    fontawesome icons  added

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -9,9 +9,8 @@ const NavbarComponent = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto Nav">
-                            <Nav.Link className="link" href="/Main">About</Nav.Link>
-                            <Nav.Link className="link" href="/Projects">Projects</Nav.Link>
-                            <Nav.Link className="link" href="/Resume">Resume</Nav.Link>
+                    <Nav.Link className="link" href='https://github.com/pratikpz18'><i className="fa fa-github" aria-hidden="true"></i></Nav.Link>
+                    <Nav.Link className="link" href='https://www.linkedin.com/in/pratik-zinjurde-2a10b71b1/'><i className="fa fa-linkedin-square" aria-hidden="true"></i></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
