825f45b4d5ff2c289d06f4ffff59946f5ad2f5d7
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-06-21 23:19:00 +0530

    twitter link added and styling done

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -11,6 +11,7 @@ const NavbarComponent = () => {
                     <Nav className="ml-auto Nav">
                     <Nav.Link className="link" href='https://github.com/pratikpz18'><i className="fa fa-github" aria-hidden="true"></i></Nav.Link>
                     <Nav.Link className="link" href='https://www.linkedin.com/in/pratik-zinjurde-2a10b71b1/'><i className="fa fa-linkedin-square" aria-hidden="true"></i></Nav.Link>
+                    <Nav.Link className="link" href='https://twitter.com/PratikZinjurde'><i className="fa fa-twitter" aria-hidden="true"></i></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
