3f12961c7a51230c5ee9fd057d27ac421d7d301d
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-06-22 21:01:50 +0530

    loading using react-spinner added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
+import { css } from "@emotion/react";
 import NavbarComponent from './components/NavbarComponent'
 import Main from './components/Main';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+
+const override = css`
+  position:absolute;
+  background-color:rgb(235, 235, 235);
+  height:100vh;
+  width:100vw;
+`;
 
 function App() {
+  const [loading,SetLoading]=useState(true);
+  useEffect(() => {
+    setTimeout(()=>{
+      SetLoading(false);
+    },2000);
+  },[]);
+
   return (
-    <div className="App">
-      <NavbarComponent></NavbarComponent>
-      <Main></Main>
-      <Projects></Projects>
-      <Resume></Resume>
-      <Contact></Contact>
+    <div>
+      {
+        loading ? 
+          ( <ClimbingBoxLoader 
+            speedMultiplier={1.25}
+            color={ 'rgb(91, 92, 95)' } 
+            loading={loading} 
+            size={30} 
+            css={override} /> )
+            : (<div className="App">
+                <NavbarComponent></NavbarComponent>
+                <Main></Main>
+                <Projects></Projects>
+                <Resume></Resume>
+                <Contact></Contact>
+              </div>)
+      }
     </div>
   );
 }
