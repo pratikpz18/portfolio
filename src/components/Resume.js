ebea51bd64245b0812a374579551d660dfa40116
pratikpz18 <zinjurdepratik.pz@gmail.com>
2023-10-21 13:09:19 +0530

    resume fix

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ const Resume = () => {
     return (
         <div className="Resume ">
             <button className="download-btn">
-                <a className="download-btn-a" href="../resume.pdf" download="Pratik Resume">
+                <a className="download-btn-a" href={process.env.PUBLIC_URL + '/resume.pdf'} download="Pratik Resume">
                 Resume   <i className="fa fa-download" aria-hidden="true"></i>
                 </a>
             </button>
